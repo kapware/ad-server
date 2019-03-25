51b09a378537edb054f744ded5bb8af3ad9f3361
Pawel Kapala <github@kapware.com>
2019-03-25 22:34:17 +0100

    Generated swagger examples

--- src/adserver/ad.clj
+++ src/adserver/ad.clj
@@ -1,34 +1,46 @@
 (ns adserver.ad
   (:require [clojure.spec.alpha     :as s]
-            [clojure.spec.gen.alpha :as gen]))
+            [clojure.spec.gen.alpha :as gen]
+            [spec-tools.core        :as st]))
 
-(s/def ::channel string?)
-(s/def ::ad-id string?)
-(s/def ::ad-ids (s/coll-of ::ad-id :kind vector?))
-(s/def ::locale (s/with-gen
-                 (s/and string? (fn [x] (= 5 (count x))))
-                 #(gen/fmap (fn [chars] (apply str chars))
-                            (gen/vector (gen/char) 5))))
-(s/def ::country (s/with-gen
-                   (s/and string? (fn [x] (= 2 (count x))))
-                   #(gen/fmap (fn [chars] (apply str chars))
-                                (gen/vector (gen/char) 2))))
-(s/def ::device string?)
-(s/def ::interest (s/with-gen
-                    (s/and string? (fn [x] (re-matches #"IAB.*" x)))
-                    #(gen/fmap (fn [x] (str "IAB" x))
-                               (gen/string))))
-(s/def ::interests (s/coll-of ::interest :kind vector?))
+
+;; TODO: a macro, or move it to coercion code?
+(defn with-example [spec]
+  (st/spec {:spec spec
+            :json-schema/example (gen/generate (s/gen spec))}))
+
+
+(s/def ::channel (with-example string?))
+(s/def ::ad-id   (with-example string?))
+(s/def ::ad-ids  (with-example (s/coll-of ::ad-id :kind vector?)))
+(s/def ::locale  (with-example (s/with-gen
+                                 (s/and string? (fn [x] (= 5 (count x))))
+                                 #(gen/fmap (fn [chars] (apply str chars))
+                                            (gen/vector (gen/char) 5)))))
+(s/def ::country (with-example
+                   (s/with-gen
+                     (s/and string? (fn [x] (= 2 (count x))))
+                     #(gen/fmap (fn [chars] (apply str chars))
+                                (gen/vector (gen/char) 2)))))
+(s/def ::device   (with-example string?))
+(s/def ::interest (with-example
+                    (s/with-gen
+                      (s/and string? (fn [x] (re-matches #"IAB.*" x)))
+                      #(gen/fmap (fn [x] (str "IAB" x))
+                                 (gen/string)))))
+(s/def ::interests (with-example
+                     (s/coll-of ::interest :kind vector?)))
 
 
 ;; Needs a better domain name, it could've been named `bannana` for now
 (s/def ::request (s/keys :req-un [::channel ::locale ::country ::device ::interests]
                          :opt-un [::ad-ids]))
 
-(s/def ::ad-content-url string?)
+(s/def ::ad-content-url (with-example string?))
 
 (s/def ::ad-creative (s/keys :req-un [::ad-id
                                       ::ad-content-url]))
 
 ;; Figure out a better name
-(s/def ::response (s/coll-of ::ad-creative :kind vector?))
+(s/def ::response (with-example
+                    (s/coll-of ::ad-creative :kind vector?)))
